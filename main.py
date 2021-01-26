0ad3613af8582d7b8e3919431b99251752cf2817
kclimb <22298376+kclimb@users.noreply.github.com>
2021-01-25 23:23:04 -0500

    cleaned up redundancies in language timing routines, added functionality for calculating timing by tag and by individual textbox

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ def TextboxCount(lines): #this function takes as input a list of lines, and retu
         n.append(len(x)-1) #is off by 1 for every line due to counting the line break
     return n, l;
 
-def TextboxLength(lines): #function takes lines as input and times how long the textbox will last
+def TextboxLengthNonJP(lines): #function takes lines as input and times how long the textbox will last
     n, l = TextboxCount(lines)
-    if l == 1: #the length is max between time it takes for the blue line to appear and time it takes for all the characters to appear
+    if l == 0:
+        return 0
+    elif l == 1: #the length is max between time it takes for the blue line to appear and time it takes for all the characters to appear
         chara_time = 1 + 2*4 + 3 + (n[0]-5)*2 #1 QF for 1st and 2nd, 4QFs for 3rd and 4th, 3 QFs for 5th, 2 for the remaining characters in the line
         band_time_togo = 28 #ceil(1.109/.04)
         time = max(chara_time, band_time_togo)
@@ -27,29 +29,14 @@ def TextboxLength(lines): #function takes lines as input and times how long the 
         remaining_band_timer = max(1.0, band_timer)
         band_time_togo = ceil((remaining_band_timer+0.109)/0.04)
         time = chara_time_body + max(chara_time_tail, band_time_togo)
-    time += 1.5 #avg timeloss from potentially starting on any QF
+    #time += 1.5 #avg timeloss from potentially starting on any QF
     return time
-
-
-def LanguageTime(contents):
-    total_time = 0
-    curr_lines = [] #represents the textbox currently being read
-    for i in range(0, len(contents)): #iterate through all the lines
-        if contents[i][0] != "/":    #test if we are in a textbox or at the end of one
-            curr_lines.append(contents[i])  #we are in the middle of a textbox so we add the line to the working set of lines
-        else: #we are on a / so at the end of 1 textbox and at the start of the next. we add the time spent on the box and move empty the current working lines
-            if curr_lines:
-                print(curr_lines)
-                print(TextboxCount(curr_lines))
-                print("\n")
-                total_time += TextboxLength(curr_lines)
-            curr_lines = []
-    return total_time
-
 
 def TextboxLengthJP(lines): #function takes lines as input and times how long the textbox will last
     n, l = TextboxCount(lines)
-    if l == 1: #the length is max between time it takes for the blue line to appear and time it takes for all the characters to appear
+    if l == 0:
+        return 0
+    elif l == 1: #the length is max between time it takes for the blue line to appear and time it takes for all the characters to appear
         chara_time = 1 + 10 + 8 + 4 + (n[0]-4)*3 #1 QF for 1st, 10QFs for 2nd, 8QF for 3rd, 4 QFs for 4th, 3 for the remaining characters in the line
         band_time_togo = 28 #ceil(1.109/.04)
         time = max(chara_time, band_time_togo)
@@ -67,22 +54,49 @@ def TextboxLengthJP(lines): #function takes lines as input and times how long th
         remaining_band_timer = max(1.0, band_timer)
         band_time_togo = ceil((remaining_band_timer+0.109)/0.04)
         time = chara_time_body + max(chara_time_tail, band_time_togo)
-    time += 1.5 #avg timeloss from potentially starting on any QF
+    #time += 1.5 #avg timeloss from potentially starting on any QF
     return time
 
 
-def LanguageTimeJP(contents):
+def LanguageTime(contents, isJP):
     total_time = 0
+    tag_times = {}
+    box_times = {}
     curr_lines = [] #represents the textbox currently being read
-    for i in range(0, len(contents)): #iterate through all the lines
-        if contents[i][0] != "/":    #test if we are in a textbox or at the end of one
-            curr_lines.append(contents[i])  #we are in the middle of a textbox so we add the line to the working set of lines
-        else: #we are on a / so at the end of 1 textbox and at the start of the next. we add the time spent on the box and move empty the current working lines
-            if curr_lines:
-                total_time += TextboxLengthJP(curr_lines)
-            curr_lines = []
-    return total_time
+    for line in contents: #iterate through all the lines
+        if line.startswith("/"):    #test if we are in a textbox or at the end of one
+            #we are on a / so at the end of 1 textbox and at the start of the next.
+            #we add the time spent on the box and move empty the current working lines
+            tag_end = line.find(":")
+            if tag_end == -1:
+                tag_end = len(line) # if line doesn't have a tag delimiter just assume the whole annotation is the tag
+            tag = line[:tag_end]
 
-f = open("FormattedJapanese.txt","r")
-contents = f.readlines()
-print(LanguageTimeJP(contents))
+            # time the textbox using the appropriate language mechanics
+            if isJP:
+                box_time = TextboxLengthJP(curr_lines)
+            else:
+                box_time = TextboxLengthNonJP(curr_lines)
+
+            # update our various timing data
+            if tag in tag_times:
+                tag_times[tag] += box_time
+            else:
+                tag_times[tag] = box_time
+            box_times[line] = box_time
+            total_time += box_time
+            curr_lines = []
+        else:
+            curr_lines.append(line)  #we are in the middle of a textbox so we add the line to the working set of lines
+    return total_time, tag_times, box_times
+
+f = open("AnnotatedJapanese.txt","r")
+contentsJ = f.readlines()
+totalJ, tagJ, boxJ = LanguageTime(contentsJ, True)
+print(totalJ)
+f = open("AnnotatedEnglish.txt","r")
+contentsE = f.readlines()
+totalE, tagE, boxE = LanguageTime(contentsE, False)
+print(totalE)
+for tag in set(tagJ.keys()).union(set(tagE.keys())):
+    print(tag, tagJ[tag], tagE[tag])
